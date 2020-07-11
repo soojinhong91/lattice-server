d3421a833f7e1ddf4c469e2b4301d1c2a5d7b26e
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-11 21:32:52 +1000

    only made small changes to associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,50 @@ Card.destroy_all
 c1 = Card.create :name => 'To Do'
 c2 = Card.create :name => 'Doing'
 c3 = Card.create :name => 'Done'
+c4 = Card.create :name => 'To Do'
+c5 = Card.create :name => 'Doing'
+c6 = Card.create :name => 'Done'
 puts "#{ Card.count } cards created."
 
 Task.destroy_all
-Task.create :description => 'What Language?'
-Task.create :description => 'Use Bootstrap?'
-Task.create :description => 'Where to Deploy?'
-Task.create :description => 'Datastructure?'
-Task.create :description => 'Any APIs?'
-Task.create :description => 'How many people working on?'
-Task.create :description => 'Any Libraries?'
-Task.create :description => 'How many days to work?'
-Task.create :description => 'One page app?'
-Task.create :description => 'Use Scaffold?'
-Task.create :description => 'Never forget git push'
-Task.create :description => 'Any missing features?'
+t1 = Task.create :description => 'What Language?'
+t2 = Task.create :description => 'Use Bootstrap?'
+t3 = Task.create :description => 'Where to Deploy?'
+t4 = Task.create :description => 'Datastructure?'
+t5 = Task.create :description => 'Any APIs?'
+t6 = Task.create :description => 'How many people working on?'
+t7 = Task.create :description => 'Any Libraries?'
+t8 = Task.create :description => 'How many days to work?'
+t9 = Task.create :description => 'One page app?'
+t10 = Task.create :description => 'Use Scaffold?'
+t11 = Task.create :description => 'Never forget git push'
+t12 = Task.create :description => 'Any missing features?'
 puts "#{ Task.count } tasks created."
+
+
+
+
+# ===================== Associations ====================================
+
+puts "Tasks and Cards "
+c1.tasks << t1 << t2
+c2.tasks << t3 << t4
+c3.tasks << t5 << t6
+c4.tasks << t8 << t7
+c5.tasks << t9 << t10
+c6.tasks << t11 << t12
+
+
+puts "Cards and Projects"
+p1.cards << c1 << c2
+p2.cards << c3
+p3.cards << c4
+p4.cards << c5
+p5.cards << c6
+
+puts "Projects and Users"
+u1.projects << p3
+u2.projects << p2
+u3.projects << p1
+u4.projects << p4
+u5.projects << p5
