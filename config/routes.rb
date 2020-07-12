5a35f671c0fd4d892d7867ed4fa9a521aeec3793
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-13 09:37:48 +1000

    Monday

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  resources :users
-  resources :tasks
-  resources :cards
-  resources :projects
-=======
   root :to => 'pages#home'
   resources :users
   resources :projects
   resources :cards
   resources :tasks
->>>>>>> fafc353ea76dc6ed13427bd09d90ca57e68ec51c
 
 end
