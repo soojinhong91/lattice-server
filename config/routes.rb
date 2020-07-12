59d77c5679e5c9dc127511ca2f81c8325281742e
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-12 15:48:11 +1000

    task CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'tasks/show'
-  get 'cards/index'
-  get 'cards/new'
-  get 'cards/edit'
-  get 'cards/show'
-  get 'projects/index'
-  get 'projects/new'
-  get 'projects/edit'
-  get 'projects/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :tasks
+  resources :cards
+  resources :projects
+
 end
