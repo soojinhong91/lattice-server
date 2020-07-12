38679c03285f8ced83202555438d4cfff1d94826
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-12 14:08:39 +1000

    controllers made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'tasks/show'
+  get 'cards/index'
+  get 'cards/new'
+  get 'cards/edit'
+  get 'cards/show'
+  get 'projects/index'
+  get 'projects/new'
+  get 'projects/edit'
+  get 'projects/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
