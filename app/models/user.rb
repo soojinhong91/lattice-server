754d2bf0677020ada2bc01b2b7bdb82ea9860048
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-13 20:45:33 +1000

    json created for projects and cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
-  validates :name, :presence => true, :uniqueness => true, :length => { minimum: 6 }
+  validates :name, :presence => true, :uniqueness => true, :length => { minimum: 4 }
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
