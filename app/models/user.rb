5f7b955dc317070ff2d7e24e28d68ae4d9075b54
Soojin Hong <soojinhong@Soojins-MacBook-Pro.local>
2020-07-13 10:59:55 +1000

    login done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :projects
+
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
-  has_and_belongs_to_many :projects
+  validates :username, :presence => true, :uniqueness => true, :length => { minimum: 6 }
+  
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   root :to => 'pages#home'
-  resources :users
+
+  # get 'sessions/create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+  resources :users, only: [:create, :show, :index]
   resources :projects
   resources :cards
   resources :tasks
