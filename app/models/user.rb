52b08347b0d0fce8b31f136baa1829f290962ee6
Jonathan C MacMillan <jmacmill@gmail.com>
2020-07-13 12:41:28 +1000

    some login bugs fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
-  validates :username, :presence => true, :uniqueness => true, :length => { minimum: 6 }
-  
+  validates :name, :presence => true, :uniqueness => true, :length => { minimum: 6 }
+
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
